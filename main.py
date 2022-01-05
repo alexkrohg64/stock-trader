4d24b7455f88947239fedaf8ad96558c6be5f99f
Lexington Krash <akrohg@yahoo.com>
2022-01-04 19:28:30 -0500

    Fetch all valid asset symbols

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from alpaca_trade_api.rest import REST, TimeFrame
 
 api = REST()
-bars = api.get_bars(symbol="AAPL", timeframe=TimeFrame.Day, start="2021-06-01", end="2021-06-11")
 
-print(len(bars))
+assets = api.list_assets(status='active', asset_class='us_equity')
+symbols = [asset.symbol for asset in assets if asset.tradable]
+print(len(symbols))
