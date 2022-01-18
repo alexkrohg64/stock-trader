87f375d8cd88824a2c56f4650aa0946bd80e99e3
Lexington Krash <akrohg@yahoo.com>
2022-01-17 20:36:18 -0500

    Calculate all values, begin saving data

--- data/tracked_asset.py
+++ data/tracked_asset.py
@@ -4,6 +4,8 @@ MACD_LONG_PERIOD = 26
 MACD_SHORT_PERIOD = 12
 MACD_SIGNAL_PERIOD = 9
 RSI_PERIOD = 14
+RSI_QUEUE = 5
+TREND_QUEUE = 20
 VOLUME_THRESHOLD = 5000000
 
 SMOOTH_9 = 0.2
@@ -15,9 +17,15 @@ class TrackedAsset:
     """Custom asset class for tracking technical analysis data"""
     def __init__(self, code):
         self.symbol = code
+        self.ema_short = 0.0
+        self.ema_long = 0.0
         self.macd = 0.0
         self.macd_signal = 0.0
-        self.rsi = 0.0
+        self.average_gains = 0.0
+        self.average_losses = 0.0
+        self.rsi = []
+        self.ema_big_long = 0.0
+        self.trend = []
 
     def __repr__(self):
         return self.symbol
@@ -25,37 +33,77 @@ class TrackedAsset:
     def __str__(self):
         return self.symbol
 
-    def calculate_macd(self, bars):
-        """Calculate MACD-related values"""
-        prices = [candle.c for candle in bars]
+    def calculate_ema_big_long(self, prices):
+        """Calculate long-period trend line"""
+        self.ema_big_long = sum(prices[0:EMA_BIG_LONG_PERIOD]) / EMA_BIG_LONG_PERIOD
 
-        ema_short = sum(prices[0:MACD_SHORT_PERIOD]) / MACD_SHORT_PERIOD
-        ema_long = sum(prices[0:MACD_LONG_PERIOD]) / MACD_LONG_PERIOD
+        for i in range(EMA_BIG_LONG_PERIOD, len(prices) - TREND_QUEUE):
+            self.ema_big_long = (prices[i] - self.ema_big_long) * SMOOTH_200 + self.ema_big_long
+
+        for i in range(len(prices) - TREND_QUEUE, len(prices)):
+            self.ema_big_long = (prices[i] - self.ema_big_long) * SMOOTH_200 + self.ema_big_long
+            self.trend.append(prices[i] > self.ema_big_long)
+
+        print('EMA-200: ' + repr(self.ema_big_long))
+        print(self.trend)
+
+    def calculate_macd(self, prices):
+        """Calculate MACD-related values"""
+        self.ema_short = sum(prices[0:MACD_SHORT_PERIOD]) / MACD_SHORT_PERIOD
+        self.ema_long = sum(prices[0:MACD_LONG_PERIOD]) / MACD_LONG_PERIOD
         macd_signal_sum = 0.0
 
         for i in range(MACD_SHORT_PERIOD, MACD_LONG_PERIOD):
-            ema_short = (bars[i].c - ema_short) * SMOOTH_12 + ema_short
+            self.ema_short = (prices[i] - self.ema_short) * SMOOTH_12 + self.ema_short
 
-        for i in range(MACD_LONG_PERIOD, len(bars)):
-            ema_short = (bars[i].c - ema_short) * SMOOTH_12 + ema_short
-            ema_long = (bars[i].c - ema_long) * SMOOTH_26 + ema_long
+        for i in range(MACD_LONG_PERIOD, len(prices)):
+            self.ema_short = (prices[i] - self.ema_short) * SMOOTH_12 + self.ema_short
+            self.ema_long = (prices[i] - self.ema_long) * SMOOTH_26 + self.ema_long
 
             if i < MACD_LONG_PERIOD + MACD_SIGNAL_PERIOD:
-                macd_signal_sum += ema_short - ema_long
+                macd_signal_sum += self.ema_short - self.ema_long
                 if i == MACD_LONG_PERIOD + MACD_SIGNAL_PERIOD - 1:
                     self.macd_signal = macd_signal_sum / MACD_SIGNAL_PERIOD
             else:
-                self.macd_signal = ((ema_short - ema_long - self.macd_signal) * SMOOTH_9
+                self.macd_signal = ((self.ema_short - self.ema_long - self.macd_signal) * SMOOTH_9
                                     + self.macd_signal)
-                if i == len(bars) - 1:
-                    print('MACD: ' + repr(ema_short - ema_long))
-                    print('MACD Signal: ' + repr(self.macd_signal))
-        print(self.symbol)
-        self.macd = ema_short - ema_long
 
-    def calculate_rsi(self, bars):
+        self.macd = self.ema_short - self.ema_long
+        print(self.symbol)
+        print('MACD: ' + repr(self.macd))
+        print('Signal: ' + repr(self.macd_signal))
+
+    def calculate_rsi(self, prices):
         """Calculate RSI-related values"""
-        self.rsi = bars[0].c
+        sum_gains = 0.0
+        sum_losses = 0.0
+
+        for i in range(RSI_PERIOD):
+            change = prices[i+1] - prices[i]
+
+            if change >= 0:
+                sum_gains += change
+            else:
+                sum_losses -= change
+
+        self.average_gains = sum_gains / RSI_PERIOD
+        self.average_losses = sum_losses / RSI_PERIOD
+
+        for i in range(RSI_PERIOD, len(prices) - 1):
+            change = prices[i+1] - prices[i]
+
+            if change >= 0:
+                self.average_gains = ((RSI_PERIOD - 1) * self.average_gains + change) / RSI_PERIOD
+                self.average_losses = (RSI_PERIOD - 1) * self.average_losses / RSI_PERIOD
+            else:
+                self.average_gains = (RSI_PERIOD - 1) * self.average_gains / RSI_PERIOD
+                self.average_losses = ((RSI_PERIOD - 1) * self.average_losses - change) / RSI_PERIOD
+
+            if i > (len(prices) - RSI_QUEUE - 2):
+                rsi_value = 100 - 100 / (1 + self.average_gains / self.average_losses)
+                self.rsi.append(rsi_value)
+
+        print('RSI: ' + repr(self.rsi))
 
     @staticmethod
     def has_enough_volume(bars):
--- initial_dataload.py
+++ initial_dataload.py
@@ -1,12 +1,13 @@
 """Perform initial dataload"""
 import datetime
+import json
 from time import sleep
 # Non-standard imports
 from alpaca_trade_api.rest import REST, TimeFrame
 from pandas.tseries.offsets import BDay
 from data import tracked_asset
 
-DATA_POINTS = 200
+DATA_POINTS = 300
 
 tracked_assets = []
 
@@ -15,7 +16,7 @@ def import_asset(code, start_date, end_date):
     bars = api.get_bars(symbol=code, timeframe=TimeFrame.Day, start=start_date, end=end_date)
 
     # skip assets which have not been on the market long enough
-    if len(bars) < DATA_POINTS / 2:
+    if len(bars) < DATA_POINTS:
         return
 
     if len(bars) > DATA_POINTS:
@@ -26,8 +27,11 @@ def import_asset(code, start_date, end_date):
     if not asset.has_enough_volume(bars):
         return
 
-    asset.calculate_macd(bars)
-    asset.calculate_rsi(bars)
+    prices = [candle.c for candle in bars]
+
+    asset.calculate_macd(prices)
+    asset.calculate_rsi(prices)
+    asset.calculate_ema_big_long(prices)
     tracked_assets.append(asset)
 
 api = REST()
@@ -36,12 +40,12 @@ assets = api.list_assets(status='active', asset_class='us_equity')
 symbols = [asset.symbol for asset in assets if asset.tradable]
 
 yesterday = datetime.date.today() - datetime.timedelta(days=1)
-starting_date = (yesterday - BDay(DATA_POINTS + 5)).strftime("%Y-%m-%d")
+starting_date = (yesterday - BDay(DATA_POINTS + 11)).strftime("%Y-%m-%d")
 
 for symbol in symbols:
     import_asset(symbol, starting_date, yesterday)
     # API free-rate limit: 200/min
     sleep(0.3)
 
-print(tracked_assets)
 print(len(tracked_assets))
+json.dump(tracked_assets, 'data/assets.json')
