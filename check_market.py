93d54e048fea0ffea31198d482906513731b6139
Alex Krohg <22198918+alexkrohg64@users.noreply.github.com>
2022-10-17 17:25:23 -0400

    Update Alpaca API library, Enable role based access to mongodb

--- check_market.py
+++ check_market.py
@@ -1,23 +1,25 @@
 """Determine and record if the stock market is open today"""
 import boto3
 import datetime
-import json
 import os
-from alpaca_trade_api.rest import REST
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import GetCalendarRequest
 from base64 import b64decode
 import telegram
+import pymongo
+import urllib
 
 LAMBDA_FUNCTION_NAME = os.environ['AWS_LAMBDA_FUNCTION_NAME']
 ID_ENCRYPTED = os.environ['APCA_API_KEY_ID']
 # Decrypt code should run once and variables stored outside of the function
 # handler so that these are decrypted once per container
-os.environ['APCA_API_KEY_ID'] = boto3.client('kms').decrypt(
+ID_DECRYPTED = boto3.client('kms').decrypt(
     CiphertextBlob=b64decode(ID_ENCRYPTED),
     EncryptionContext={'LambdaFunctionName': LAMBDA_FUNCTION_NAME}
 )['Plaintext'].decode('utf-8')
 
 KEY_ENCRYPTED = os.environ['APCA_API_SECRET_KEY']
-os.environ['APCA_API_SECRET_KEY'] = boto3.client('kms').decrypt(
+KEY_DECRYPTED = boto3.client('kms').decrypt(
     CiphertextBlob=b64decode(KEY_ENCRYPTED),
     EncryptionContext={'LambdaFunctionName': LAMBDA_FUNCTION_NAME}
 )['Plaintext'].decode('utf-8')
@@ -34,25 +36,34 @@ CHAT_DECRYPTED = boto3.client('kms').decrypt(
     EncryptionContext={'LambdaFunctionName': LAMBDA_FUNCTION_NAME}
 )['Plaintext'].decode('utf-8')
 
+MONGO_ENCRYPTED = os.environ['MONGO_CONNECTION_STRING']
+MONGO_DECRYPTED = boto3.client('kms').decrypt(
+    CiphertextBlob=b64decode(MONGO_ENCRYPTED),
+    EncryptionContext={'LambdaFunctionName': LAMBDA_FUNCTION_NAME}
+)['Plaintext'].decode('utf-8')
+
 def lambda_handler(event, context):
-    api = REST()
+    alpaca_client = TradingClient(api_key=ID_DECRYPTED, secret_key=KEY_DECRYPTED, paper=False)
     telegram_bot = telegram.Bot(token=BOT_DECRYPTED)
 
     today = datetime.date.today()
+    today_filter = GetCalendarRequest(start=today, end=today)
 
-    trading_day = api.get_calendar(start=today, end=today)
+    trading_calendar = alpaca_client.get_calendar(filters=today_filter)
 
-    if len(trading_day) != 1:
-        error_message = 'Unexpected number of trading days returned! : ' + repr(len(trading_day))
+    if len(trading_calendar) != 1:
+        error_message = 'Unexpected number of trading days returned! : ' + repr(len(trading_calendar))
         telegram_bot.send_message(text=error_message, chat_id=CHAT_DECRYPTED)
         return
 
-    market_is_open = trading_day[0].date.date() == today
+    market_is_open = trading_calendar[0].date == today
 
-    table = boto3.resource('dynamodb').Table('asset_list')
-    table.load()
-    table.put_item(Item={
-        'symbol': 'MARKET_IS_OPEN',
-        'market_is_open': market_is_open,
-        'day_of_month': today.day
-    })
+    session_encoded = urllib.parse.quote_plus(os.environ.get('AWS_SESSION_TOKEN'))
+    mongo_connection_string = MONGO_DECRYPTED + session_encoded
+
+    mongo_client = pymongo.MongoClient(mongo_connection_string)
+    mongo_db = mongo_client['stocks']
+    mongo_collection = mongo_db['MARKET_IS_OPEN']
+    market_dict = {'market_is_open': market_is_open, 'day_of_month': today.day}
+    mongo_collection.insert_one(document=market_dict)
+    mongo_client.close()
